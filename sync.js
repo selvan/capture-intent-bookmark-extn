228c3a89b047aaa796ab4918ede0876cb8de1cf8
selvan <msquare.irc@gmail.com>
2012-12-16 21:24:55 +0530

    search term & url extractor

---
+++ sync.js
@@ -0,0 +1,33 @@
+//based on "Event Page Example" at https://developer.chrome.com/extensions/samples.html
+
+var search_term;
+
+function checkForSearchURL(tabId, changeInfo, tab) {
+    var gmatch = tab.url.match(/^https?:\/\/([a-zA-Z\d\.-]*)google\.([a-zA-Z\d\.-]+)\/.+/gi)
+    if(gmatch !== null) {
+        var queryString = {};
+        tab.url.replace(
+            new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+            function($0, $1, $2, $3) { queryString[$1] = $3; }
+        );
+        search_term = queryString["q"];
+    }
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForSearchURL);
+
+//user click on icon
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.windows.getCurrent(function(win) {
+        chrome.tabs.getSelected(win.id, actionClicked);
+    });
+});
+
+
+function actionClicked(tab) {
+    console.log(tab.url);
+}
+
+
+
